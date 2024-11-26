cba83b516e665a6d7d6bc4c90d3a4cabf48a9c9c
adivenxnataly <adivenxnataly@gmail.com>
2024-11-27 05:18:14 +0700

    init commit

---
+++ afmtk.sh
@@ -0,0 +1,14 @@
+[ ! "$MODPATH" ] && MODPATH=${0%/*}
+
+MODPOL=`find $MODPATH -type f -name audio_policy.conf`
+MODCONF=`find $MODPATH -type f -name audio_policy_configuration.xml`
+
+# enable high-sampling rate options up to 192kHz:
+for MODCONFIG in $MODCONF; do
+    sed -i 's|samplingRates="44100 48000"|samplingRates="44100 48000 96000 192000"|' $MODCONF
+done
+
+# Enable Dynamic Range Control (DRC) if disable:
+for MODCONFIG in $MODCONF; do
+    sed -i '/speaker_drc_enabled="false"/s/false/true/' $MODCONF
+done
---
+++ copy.sh
@@ -0,0 +1,26 @@
+[ ! "$MODPATH" ] && MODPATH=${0%/*}
+
+# function
+copy_policy_file() {
+  mkdir -p `dirname "$2"`
+  cp -af "$1" "$2"
+}
+
+# audio file
+AUD="audio_policy.conf -o -name audio_policy_configuration.xml"
+rm -f `find $MODPATH -type f -name $AUD`
+FILES=`find /system -type f -name $AUD`
+for FILE in $FILES; do
+  MODFILE=$MODPATH/system`echo "$FILE" | sed 's|/system||g'`
+  copy_policy_file $FILE $MODFILE
+done
+FILES=`find /vendor -type f -name $AUD`
+for FILE in $FILES; do
+  if [ -L $MODPATH/system/vendor ]\
+  && [ -d $MODPATH/vendor ]; then
+    MODFILE=$MODPATH$FILE
+  else
+    MODFILE=$MODPATH/system$FILE
+  fi
+  copy_policy_file $FILE $MODFILE
+done
