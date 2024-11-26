15e3d412519faf4a6615afa0b3d9eaffe767a97c
Adinata <adinata.ch@gmail.com>
2024-11-27 05:30:39 +0700

    high-resolution audio for MediaTek devices

--- afmtk.sh
+++ afmtk.sh
@@ -7,8 +7,3 @@ MODCONF=`find $MODPATH -type f -name audio_policy_configuration.xml`
 for MODCONFIG in $MODCONF; do
     sed -i 's|samplingRates="44100 48000"|samplingRates="44100 48000 96000 192000"|' $MODCONF
 done
-
-# Enable Dynamic Range Control (DRC) if disable:
-for MODCONFIG in $MODCONF; do
-    sed -i '/speaker_drc_enabled="false"/s/false/true/' $MODCONF
-done
